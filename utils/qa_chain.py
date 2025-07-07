73ea12023b5a176e6b3c4bc66d0129d5f415a5f2
amohanty6 <arshiamohanty26@gmail.com>
2025-07-07 17:00:14 +0530

    Create qa_chain.py

---
+++ utils/qa_chain.py
@@ -0,0 +1,15 @@
+# utils/qa_chain.py
+from langchain.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.chains import RetrievalQA
+from langchain.llms import HuggingFaceHub  # Use any HF model
+
+def get_chain():
+    db = FAISS.load_local("vector_store", HuggingFaceEmbeddings())
+    retriever = db.as_retriever()
+
+    qa = RetrievalQA.from_chain_type(
+        llm=HuggingFaceHub(repo_id="google/flan-t5-large", model_kwargs={"temperature": 0.3, "max_length": 256}),
+        retriever=retriever
+    )
+    return qa
