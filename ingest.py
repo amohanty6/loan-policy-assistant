55ae20debc0620b8d5441369d65ce4f3ab7b3363
amohanty6 <arshiamohanty26@gmail.com>
2025-07-13 11:50:19 +0530

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -1,7 +1,7 @@
 import os
 from langchain_community.document_loaders import TextLoader, PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 
 DATA_FOLDER = "data"
