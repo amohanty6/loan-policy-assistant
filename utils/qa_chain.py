365b6d0c1e42cf780b34d362cbed7c2fb9bf2029
amohanty6 <arshiamohanty26@gmail.com>
2025-07-13 11:51:22 +0530

    Update qa_chain.py

--- utils/qa_chain.py
+++ utils/qa_chain.py
@@ -1,6 +1,6 @@
 # utils/qa_chain.py
-from langchain.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.chains import RetrievalQA
 from langchain.llms import HuggingFaceHub  # Use any HF model
 
